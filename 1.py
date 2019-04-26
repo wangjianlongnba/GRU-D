56793e6305dfb89013218af121a05b9cc514d958
wangjianlong <test@wangjianlong.com>
2019-04-26 17:34:43 +0800

    Update README.md aaaa

---
+++ 1.py
@@ -0,0 +1,13 @@
+import torch
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+
+x = torch.randn(1)
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    y = torch.ones_like(x, device=device)
+    x = x.to(device)
+    z = x + y
+    print(z)
+    print(z.to("cpu", torch.double))
